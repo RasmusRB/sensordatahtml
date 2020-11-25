bce38fc2635fa0e07ef8e82594083302a8215d66
RasmusRB <rasmusbergstroem87@gmail.com>
2020-11-25 21:34:45 +0100

    fake sensor data web

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,20 +1,42 @@
+import axios, {
+    AxiosResponse,
+    AxiosError
+} from "../../node_modules/axios/index"
+
+interface ISensorData{
+    id: number
+    sensorName: string
+    temperature: number
+    co2: number
+}
+
+let baseUrl: string = "https://rb-sensordata-rest.azurewebsites.net/api/sensor/"
+
 new Vue({
     // TypeScript compiler complains about Vue because the CDN link to Vue is in the html file.
     // Before the application runs this TypeScript file will be compiled into bundle.js
     // which is included at the bottom of the html file.
     el: "#app",
     data: {
-        name: "",
-        greeting: ""
+        sensordata: [],
+        idToGetBy: -1
     },
     methods: {
-        sayHello() {
-            console.log("Say Hello " + this.name)
-            if (this.name == "") {
-                this.greeting = "Hello NoName"
-            }
-            else {
-                this.greeting = "Hello " + this.name
+        helperGetAndShow(url: string) { // helper method to get all
+            axios.get<ISensorData[]>(url)
+            .then((response: AxiosResponse<ISensorData[]>) => {
+                this.sensordata = response.data
+            })
+            .catch((error: AxiosError) => {
+                alert(error.message)
+            })
+        },
+        getAllData(){
+            this.helperGetAndShow(baseUrl)
+        },
+        clearAllData(){
+            while(this.sensordata.length > 0){
+                this.sensordata.pop()
             }
         }
     }
